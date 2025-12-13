e702e365aa98c74f0ffd604a8f2b8391d9051149
AlexNagore <alexnagoreiri@gmail.com>
2025-12-13 21:34:54 +0100

    [] - maquina funcional

--- trabajofinal/machine/src/prepare.sh
+++ trabajofinal/machine/src/prepare.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo "--- 1. Instalando Dependencias ---"
-# FIX: Añadimos 'binutils' (CRÍTICO para que chkrootkit funcione)
 apt-get update && apt-get install -y \
     lighttpd \
     lighttpd-mod-webdav \
@@ -10,11 +9,9 @@ apt-get update && apt-get install -y \
     cron \
     net-tools \
     curl \
-    vim \
     procps \
-    sudo \
     binutils \
-    && apt-get clean
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 echo "--- 2. Configurando PHP ---"
 lighty-enable-mod fastcgi
@@ -23,9 +20,9 @@ lighty-enable-mod fastcgi-php
 echo "--- 3. Desplegando Web Original ---"
 rm -rf /var/www/html/*
 cp -r /opt/html/* /var/www/html/
-
-# FIX: Creamos la carpeta explícitamente para evitar error "No such file"
 mkdir -p /var/www/html/test
+
+usermod -s /bin/bash www-data
 
 chown -R www-data:www-data /var/www/html
 chmod 777 /var/www/html/test
@@ -33,12 +30,12 @@ chmod 777 /var/www/html/test
 echo "--- 4. Configurando Lighttpd ---"
 cp /opt/conf/lighttpd.conf /etc/lighttpd/lighttpd.conf
 
-# Carpetas de sistema y logs
 mkdir -p /var/run/lighttpd
 chown -R www-data:www-data /var/run/lighttpd
-chmod 777 /var/tmp
 
-# Creamos el log vacío
+chmod 1777 /tmp
+chmod 1777 /var/tmp
+
 mkdir -p /var/log/lighttpd
 touch /var/log/lighttpd/error.log
 chown -R www-data:www-data /var/log/lighttpd
@@ -49,11 +46,18 @@ chmod +x /usr/sbin/chkrootkit
 
 cp /opt/conf/cron_exploit /etc/cron.d/chkrootkit
 chmod 644 /etc/cron.d/chkrootkit
+
 touch /etc/cron.d/chkrootkit
 
 echo "--- 6. Configurando Usuarios ---"
 useradd -m -s /bin/bash sickos
 echo "sickos:sickos" | chpasswd
 echo "root:sickos" | chpasswd
+
+echo "--- 7. Preparando Flags ---"
+cp /opt/flag/user.txt /home/sickos/user.txt
+chown sickos:sickos /home/sickos/user.txt
+chmod 755 /home/sickos
+cp /opt/flag/root.txt /root/root.txt
 
 echo "--- Instalación Completa ---"
--- trabajofinal/start.sh
+++ trabajofinal/start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > flag/user.txt
-echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > flag/root.txt
-echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > flag/flag.txt
+echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > machine/flag/user.txt
+echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > machine/flag/root.txt
+echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > machine/flag/flag.txt
 
 docker compose -p web-lab up -d --build
